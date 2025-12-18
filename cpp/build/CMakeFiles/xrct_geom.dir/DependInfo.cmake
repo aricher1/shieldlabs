c7b82ad52764e6d84b45e3598d51ac084b87e00a
aricher1 <aidan.richerrr@gmail.com>
2025-12-18 10:26:16 -0500

    added cpp logic for geometry

---
+++ cpp/build/CMakeFiles/xrct_geom.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/aidanricher/xrct-shielding/cpp/src/GeometryEngine.cpp" "CMakeFiles/xrct_geom.dir/src/GeometryEngine.cpp.o" "gcc" "CMakeFiles/xrct_geom.dir/src/GeometryEngine.cpp.o.d"
+  "/Users/aidanricher/xrct-shielding/cpp/src/main.cpp" "CMakeFiles/xrct_geom.dir/src/main.cpp.o" "gcc" "CMakeFiles/xrct_geom.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/build/CMakeFiles/xrct_geom.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xrct_geom.dir/src/GeometryEngine.cpp.o"
+  "CMakeFiles/xrct_geom.dir/src/GeometryEngine.cpp.o.d"
+  "CMakeFiles/xrct_geom.dir/src/main.cpp.o"
+  "CMakeFiles/xrct_geom.dir/src/main.cpp.o.d"
+  "xrct_geom"
+  "xrct_geom.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xrct_geom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
