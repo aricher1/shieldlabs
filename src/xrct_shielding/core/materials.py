415bf06a06a708ee3f32752d1e01c3a7707e00e9
Aidan Richer <aricher1@users.noreply.github.com>
2025-12-31 13:25:50 -0500

    remove obselete src and tests directories containing old python code

--- src/xrct_shielding/core/materials.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen = True)
-class Material:
-    name: str
-    density: float      # kg/m^3
-
--- src/xrct_shielding/core/rays.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from .geometry import Point
-
-
-@dataclass(frozen = True)
-class Ray:
-    origin: Point
-    target: Point
-    
